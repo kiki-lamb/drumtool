65f580771a12d108c96ddca3672418d99a7a590e
Kiki Lamb <katherinem13@Syn.local>
2015-11-09 17:53:42 -0500

    Initial commit.

---
+++ input.rb
@@ -0,0 +1,28 @@
+bpm 128
+loop 32
+
+instrument bd 36
+  loop 32
+  on %4
+  on 15
+  on 18
+instrument sd 37
+  shift 4
+  on %8
+instrument ch 38
+  on %3
+instrument oh 39
+  loop 16
+  muted_by ch
+  on %4
+  shift 2
+  mute
+instrument mt 46  
+  on %5
+  shift 1
+  mute
+instrument ut 46
+  loop 32
+  on %4
+  shift 2
+
