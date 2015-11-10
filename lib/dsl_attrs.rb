9e78062f4ea3e2b9b2e8b36883ce8c5d9b4dbe26
Kiki Lamb <katherinem13@Syn.local>
2015-11-10 08:34:25 -0500

    clean a bit

--- lib/dsl_attrs.rb
+++ lib/dsl_attrs.rb
@@ -2,18 +2,15 @@ module DslAttrs
   def dsl_attr name, after: [], failover: nil, default: nil
       define_method(name) do |v = nil, &b|
         if v  
-          tmp = instance_variable_get("@#{name}")
-
           instance_variable_set "@#{name}", v
           [*after].each do |after|
             send after
           end
         end
 
-        tmp = instance_variable_get("@#{name}")
+        tmp = instance_variable_get "@#{name}"
 
-        if tmp
-        elsif failover
+        if (! tmp) && failover
           tmp = send(failover).send(name, v)
         end
 
@@ -26,20 +23,15 @@ module DslAttrs
 
       define_method(name) do |v = nil, &b|
         if v  
-          tmp = instance_variable_get("@#{name}")
-
           instance_variable_set "@#{name}", v
           [*after].each do |after|
             send after
           end
         end
 
-				left = (instance_variable_get("@#{name}") || 0)
 				up_obj = send(up)
-				fetched = up_obj ? up_obj.send(name) : 0
-				right = (up ? (fetched || 0) : 0)
 
-        tmp = left + right
+        tmp = (instance_variable_get("@#{name}") || 0) + ((up_obj ? up_obj.send(name) : 0) || 0)
 				tmp = transformer.call tmp if transformer
 				tmp
       end
