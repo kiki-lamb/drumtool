a70af60545c2d2e4f5117d73c7bf1889a496e7c2
Kiki Lamb <katherinem13@Syn.local>
2015-11-14 04:49:16 -0500

    ...

--- lib/drumtool/live_coder.rb
+++ lib/drumtool/live_coder.rb
@@ -12,14 +12,14 @@ module DrumTool
 
 	  attr_reader :exception, :engine
 
-	  def initialize filename, refresh_interval: 16, preprocessor: Preprocessors::Preprocessor, logger: nil, pp_logger: nil, rescue_eval: true, output: UniMIDI::Output[0], clock: nil, reset_on_clock_stop: true
-	    @filename, @refresh_interval, @__preprocessor__, @logger, @pp_logger, @rescue_eval = filename, refresh_interval, preprocessor, logger, pp_logger, rescue_eval
-	    @__hash__, @engine, @__exception_lines__ = nil, Models::Basic.build, nil
+	  def initialize filename, refresh_interval: 16, preprocessor: Preprocessors::Preprocessor, logger: nil, pp_logger: nil, rescue_eval: true, output: UniMIDI::Output[0], clock: nil, reset_loop_on_stop: true
+	    @filename, @refresh_interval, @preprocessor, @logger, @pp_logger, @rescue_eval = filename, refresh_interval, preprocessor, logger, pp_logger, rescue_eval
+	    @hash, @engine, @exception_lines = nil, Models::Basic.build, nil
 	    @old_engine = nil
 	    @exception = false
 	    @last_line_length = 2
-			@__clock__ = clock
-			@__reset_on_clock_stop__ = reset_on_clock_stop
+			@clock = clock
+			@reset_loop_on_stop = reset_loop_on_stop
 	  end
 
 	  def play
@@ -31,10 +31,10 @@ module DrumTool
 
 			refresh
 
-			clock = Topaz::Clock.new(@__clock__ ? @__clock__ : engine.bpm, interval: 16) do
+			clock = Topaz::Clock.new(@clock ? @clock : engine.bpm, interval: 16) do
 	      begin
 	        begin
-	          @__exception_lines__ = [] unless exception
+	          @exception_lines = [] unless exception
 	          @logger.flush if @logger
 	          @pp_logger.flush if @pp_logger
 
@@ -50,18 +50,18 @@ module DrumTool
 	          refresh if (tick%@refresh_interval == 0)
 						refresh_time = (Time.now - refresh_time) * 1000
 
-						clock.tempo = engine.bpm unless @__clock__
+						clock.tempo = engine.bpm unless @clock
 	        
 	          io << refresh_time.to_s[0..4].ljust(5,"0") << " | "
 
 	          engine.play(tick, log: io) do
 	             tmp = [ (engine.tick_length - (Time.now - started_tick)), 0 ].max
-	             @__exception_lines__.unshift "DROPPED A TICK!" if 0 == tmp
+	             @exception_lines.unshift "DROPPED A TICK!" if 0 == tmp
 	             tmp
 	          end if engine
 	          started_tick = Time.now
 
-	          io << "\b#{@__exception_lines__[tick%(@engine.loop || 16)]}\n" if @__exception_lines__.any?
+	          io << "\b#{@exception_lines[tick%(@engine.loop || 16)]}\n" if @exception_lines.any?
 
 	          @last_line_length = io.string.length
 
@@ -75,7 +75,7 @@ module DrumTool
 	          end
 	          engine.close_notes if engine
 	          @exception = e
-	          @__exception_lines__ = [ "WARNING: #{@exception.to_s}", *@exception.backtrace, "" ]
+	          @exception_lines = [ "WARNING: #{@exception.to_s}", *@exception.backtrace, "" ]
 	          @engine = @old_engine
 	          @refresh_interval = @engine.refresh_interval || @refresh_interval
 	          nil
@@ -87,11 +87,11 @@ module DrumTool
 
 			clock.event.stop do 
 			  puts "STOP"
-				tick -= tick % engine.loop if @__reset_on_clock_stop__ and engine.loop
+				tick -= tick % engine.loop if @reset_loop_on_stop and engine.loop
 			  engine.close_notes
 			end
 
-			puts "Waiting for MIDI clock...\nControl-C to exit\n" unless @__clock__.nil?
+			puts "Waiting for MIDI clock...\nControl-C to exit\n" unless @clock.nil?
 			
   		clock.start
 
@@ -107,9 +107,9 @@ module DrumTool
 	      d << text
 	    end.hexdigest
 
-	    if hash != @__hash__
-	      @__hash__ = hash
-	      proc = eval "\nProc.new do\n#{@__preprocessor__.call File.open("#{@filename}").read, logger: @pp_logger}\nend"
+	    if hash != @hash
+	      @hash = hash
+	      proc = eval "\nProc.new do\n#{@preprocessor.call File.open("#{@filename}").read, logger: @pp_logger}\nend"
 	      @exception = nil
 	      @old_engine = @engine
 	      @engine = Models::Basic.build(&proc).inherit @old_engine
