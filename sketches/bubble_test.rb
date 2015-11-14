7e16868bee89bb3a865c8badc3835c811cf30948
Kiki Lamb <katherinem13@Syn.local>
2015-11-14 03:31:09 -0500

    play with topaz

--- sketches/bubble_test.rb
+++ sketches/bubble_test.rb
@@ -5,23 +5,25 @@ require_relative "../lib/drumtool"
 include DrumTool::Models::Bubbles
 
 tb = bubble do
+  rotate -1
+
   bubble do
 	  trigger { |t| 0 == t % 4 }
 	  note :bd, 36     
   end
 
-  bubble do
-	  rotate 1
-		note :sd, 37
-		trigger { |t| 0 == (t+4) % 8 }
-  end
+#  bubble do
+#	  rotate 1
+#		note :sd, 37
+#		trigger { |t| 0 == (t+4) % 8 }
+#  end
 end
 
 ################################################################################
 
 input = UniMIDI::Input[1]
 
-clock = Topaz::Clock.new(input, interval: 16) do 
+clock = Topaz::Clock.new(input, midi_transport: false, interval: 16) do 
 		puts "#{Time.now} #{tb.tick} #{tb.tick!}"
 end
 
