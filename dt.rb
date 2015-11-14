303d5f37958cdb1f7acfd64ada3fabf281ae5f9d
Kiki Lamb <katherinem13@Syn.local>
2015-11-14 00:48:46 -0500

    ...

--- dt.rb
+++ dt.rb
@@ -1,4 +1,6 @@
-require "./lib/models/drum"
+#!/usr/bin/env ruby
+require_relative "./lib/models/drum"
+
 file = "input/sample.dt"
 
 LiveCoder.play file, refresh_interval: 1, rescue_eval: false, logger: $stdout, pp_logger: File.open("output/preprocessor", "w")
