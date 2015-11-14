36fb3f63055c753c9ff0bbf515c007aa27d76610
Kiki Lamb <katherinem13@Syn.local>
2015-11-14 00:55:01 -0500

    ...

--- dt.rb
+++ dt.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 require_relative "./lib/models/basic"
 
-file = "input/sample.dt"
-
-LiveCoder.play file, refresh_interval: 1, rescue_eval: false, logger: $stdout, pp_logger: File.open("output/preprocessor", "w")
+LiveCoder.play \
+  "input/sample.dt", 
+  refresh_interval: 1, 
+	rescue_eval: false, 
+	logger: $stdout, 
+	pp_logger: File.open("output/preprocessor", "w")
