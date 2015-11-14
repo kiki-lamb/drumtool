4f167d61343efca399e505266fde3120ef4daa56
Kiki Lamb <katherinem13@Syn.local>
2015-11-14 04:42:32 -0500

    ...

--- dt.rb
+++ dt.rb
@@ -4,7 +4,7 @@ require_relative "./lib/drumtool"
 DrumTool::LiveCoder.play \
   "input/sample.dt", 
   refresh_interval: 1, 
-	rescue_eval: false, 
+	rescue_eval: true, 
 	logger: $stdout, 
 	pp_logger: File.open("output/preprocessor", "w"),
 	clock: UniMIDI::Input[1]
