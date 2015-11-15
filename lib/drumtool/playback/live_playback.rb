daf7e0f9edb53b5a46b4c477ca00c42b0e199da1
Kiki Lamb <katherinem13@Syn.local>
2015-11-15 03:30:02 -0500

    ...

--- lib/drumtool/playback/live_playback.rb
+++ lib/drumtool/playback/live_playback.rb
@@ -6,6 +6,7 @@ module DrumTool
       *a, 
       reload_interval: 16,
       rescue_exceptions: true,
+			init: Models::Basic::TimingScope.new,
 			**b
     )
 		  super *a, **b
@@ -13,7 +14,7 @@ module DrumTool
       @reload_interval = reload_interval
 			@last_reload_time = nil
 			@last_reload_tick = 0
-		  @reloader = Loader.new(@filename, @preprocessor, rescue_exceptions: rescue_exceptions, init: Models::Basic::TimingScope.new)
+		  @reloader = Loader.new(@filename, @preprocessor, rescue_exceptions: rescue_exceptions, init: init)
 			@reloader.after do |to| 
 			    @clock.tempo = to.bpm if to.bpm && @clock unless @input_clock
 			    to.bpm @clock.tempo unless to.bpm
@@ -48,6 +49,7 @@ module DrumTool
 			countdown = countdown%1.0 == 0  ? countdown.to_i : countdown.to_r
 
 		  [ 
+			  engine.class.name, 
 			  "#{unchanged_bars.to_i.to_s.rjust(2)} bars", 
 			  "T-#{countdown} bars",
 				"#{@last_reload_time.to_s[0..4].ljust(5,"0")} ms",
