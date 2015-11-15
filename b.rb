d9282b7996121d4fb9a018c93f36911c4a21003c
Kiki Lamb <katherinem13@Syn.local>
2015-11-15 05:17:35 -0500

    Succesfully read standard input file with Bubbles.

--- b.rb
+++ b.rb
@@ -4,9 +4,9 @@ require_relative "./lib/drumtool"
 include DrumTool
 include DrumTool::Models::Bubbles
 
-Preprocessors::BubblePreprocessor.log_to "output/bubble_preprocessor", $stdout
-LivePlayback.log_to                      "output/livecoder",  $stdout
+Preprocessors::BubblePreprocessor.log_to "output/bubble_preprocessor"
+LivePlayback.log_to                      "output/bubble_livecoder",  $stdout
 
-LivePlayback.start "input/bsample.dt", preprocessor: Preprocessors::BubblePreprocessor, init: track
+LivePlayback.start "input/sample.dt", preprocessor: Preprocessors::BubblePreprocessor, init: track
 
 
