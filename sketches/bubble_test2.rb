174c562fa4dc1ec2e72a64ddb06c18c6516f0bac
Kiki Lamb <katherinem13@Syn.local>
2015-11-14 19:45:36 -0500

    ..

---
+++ sketches/bubble_test2.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+require "topaz"
+require_relative "../lib/drumtool"
+
+include DrumTool::Models::Bubbles
+
+tb = bubble do
+  bubble do
+	  trigger { |t| 0 == t % 4 }
+	  note :bd, 36     
+  end
+
+  bubble do
+		note :sd, 37
+		trigger { |t| 0 == (t+4) % 8 }
+  end
+end
+
+DrumTool::Playback.start tb
+
+
+
