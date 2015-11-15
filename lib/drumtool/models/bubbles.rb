7c8b6820330dc2c53396481b3d4c38f5ca3272f9
Kiki Lamb <katherinem13@Syn.local>
2015-11-14 21:53:42 -0500

    note

--- lib/drumtool/models/bubbles.rb
+++ lib/drumtool/models/bubbles.rb
@@ -8,6 +8,13 @@ module DrumTool
 	end
 end
 
+# Current:
+# Base < Events < RelativeTime < AbsoluteTime < Engine
+#                              < Triggered    < Music 
 
-# Context < Events < RelativeTime < AbsoluteTime < Engine
-#                                 < Triggered < Music 
+# Future:
+# Base < Events < Time < AbsoluteTime
+#			 	 							 < RelativeTime < Engine
+#											                < Triggered < Music
+#
+# In this model, Engine must be generate an AbsoluteTime as a parent object when initialized.
