63b34ec913b14698bad0500474b182c3231569f2
Kiki Lamb <katherinem13@Syn.local>
2015-11-14 20:58:45 -0500

    ...

--- lib/drumtool/models/bubbles/relative_time_bubble.rb
+++ lib/drumtool/models/bubbles/relative_time_bubble.rb
@@ -1,15 +1,13 @@
 module DrumTool
 	module Models
 		module Bubbles
-		  class TimeBubble < Bubble
+		  class RelativeTimeBubble < Bubble
 		    bubble_attr :refresh_interval, default: 16
 		    bubble_attr :bpm, default: 112
 		    bubble_attr :loop, default: nil
 		    bubble_attr :rotate
 		    bubble_attr :shift
 		    bubble_attr :scale
-
-		    proximal_bubble_toggle :mute    
 
 		    def time  
 		      base = (parent ? parent.time : top.tick) 
@@ -20,9 +18,6 @@ module DrumTool
 		      e_time
 		    end
 
-		    def active? 
-		      ! mute?
-		    end
 
 				# This is needed to make WorldBubble a valid engine for Playbacks:
 				def events_at t
@@ -30,11 +25,11 @@ module DrumTool
 					events.map(&:last).tap { |x| puts "OUT => #{x}" }
 				end				
 
-		    def events force: false
+		    def events
 		      (
 		        self.children.map do |ch|
 		          ch.events
-		        end.flatten(1) if (force || active?)
+		        end.flatten(1)
 		      ) || []
 		    end
 		  end
