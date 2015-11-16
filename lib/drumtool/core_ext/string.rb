4941f2fea2414482329c4fd97b686ccc92944d0b
Kiki Lamb <katherinem13@Syn.local>
2015-11-16 04:41:18 -0500

    some shitty methods.

--- lib/drumtool/core_ext/string.rb
+++ lib/drumtool/core_ext/string.rb
@@ -2,4 +2,12 @@ class String
   def chunks length
     split('').each_slice(length).map(&:join).to_a
   end
+
+	def shitty_camelize
+    split(/_+/).map(&:capitalize).join
+	end
+
+	def shitty_underscore
+		gsub(/(?<!^)([A-Z])/) { |x| "_#{Regexp.last_match[1]}" }.downcase
+	end
 end
