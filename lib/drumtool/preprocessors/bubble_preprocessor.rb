2dbeb176c1cd524d0b15546eee19d1b893b4be02
Kiki Lamb <katherinem13@Syn.local>
2015-11-16 05:38:26 -0500

    ...

--- lib/drumtool/preprocessors/bubble_preprocessor.rb
+++ lib/drumtool/preprocessors/bubble_preprocessor.rb
@@ -1,6 +1,6 @@
 module DrumTool
   module Preprocessors
-    class BubblePreprocessor < Preprocessor
+    class BubblePreprocessor < Base
 
       abbreviate %i{
         bubble
@@ -22,15 +22,15 @@ module DrumTool
         trigger: [ :when, :on ],
         untrigger: [ :not, :except, :exclude ]
 
-				stages %i{
-				   untabify
-           strip_blank_lines_and_trailing_whitespace
-           extend_block_comments
-           strip_blank_lines_and_trailing_whitespace_and_comments
-           rubify_arguments_and_expand_abbreviations
-           rubify_pythonesque_blocks
-           objectify			
-				}
+			stages \
+				 Untabify,
+         StripBlankLinesAndTrailingWhitespace,
+         ExtendBlockComments,
+         StripBlankLinesAndTrailingWhitespaceAndComments,
+         RubifyArgumentsAndExpandAbbreviations,
+         RubifyPythonesqueBlocks,
+         Procify,
+         :objectify
 
 			def objectify
 			  self.text = "self.class.include DrumTool::Models\nBubbles.track do \n#{text}\nend"
