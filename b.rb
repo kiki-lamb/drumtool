b47234acd82467e217d4dca0aeb2a1773bcc91d6
Kiki Lamb <katherinem13@Syn.local>
2015-11-15 23:05:59 -0500

    share output filesg

--- b.rb
+++ b.rb
@@ -4,8 +4,8 @@ require_relative "./lib/drumtool"
 include DrumTool
 include DrumTool::Models::Bubbles
 
-Preprocessors::BubblePreprocessor.log_to "output/bubble_preprocessor"
-LivePlayback.log_to                      "output/bubble_livecoder",  $stdout
+Preprocessors::BubblePreprocessor.log_to "output/preprocessor"
+LivePlayback.log_to                      "output/livecoder",  $stdout
 
 LivePlayback.start "input/sample.dt", preprocessor: Preprocessors::BubblePreprocessor # , init: track
 
