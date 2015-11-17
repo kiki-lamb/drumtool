ba9b92b5ecec26bebbe42a0bc48f45275758553b
Kiki Lamb <katherinem13@Syn.local>
2015-11-17 03:04:48 -0500

    ...

--- lighter_bubbles_test.rb
+++ lighter_bubbles_test.rb
@@ -4,12 +4,12 @@ require_relative "./lib/drumtool"
 include DrumTool
 include DrumTool::Models
 
-Bubbles::Preprocessors::StrictPreprocessor.log_to "output/preprocessor", $stdout
+LighterBubbles::Preprocessors::Preprocessor.log_to "output/preprocessor", $stdout
 
 filename = ARGV[0] || "input/strict.dt"
 $stdout << "Preprocessing " << filename << "\n"
 
-Bubbles::Preprocessors::StrictPreprocessor.new(File.open(filename)).result
+LighterBubbles::Preprocessors::Preprocessor.new(File.open(filename)).result
 
 
 
