cb4f3d26d8199f0685858fdfba7903d1eefc4a8f
Kiki Lamb <katherinem13@Syn.local>
2015-11-17 18:30:49 -0500

    ..

--- bubbles.rb
+++ bubbles.rb
@@ -7,7 +7,7 @@ include DrumTool::Models
 Bubbles::Preprocessors::Preprocessor.log_to "output/preprocessor"
 LivePlayback.log_to       "output/livecoder",  $stdout
 
-filename = ARGV[0] || "input/sample3.dt"
+filename = ARGV[0] || "input/bubbles.dt2"
 $stdout << "Begin playback of " << filename << "\n"
 
 LivePlayback.start filename, preprocessor: Bubbles::Preprocessors::Preprocessor.new, init: Bubbles.track
