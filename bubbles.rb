81efc640968d92448a3a22995e80a6f2cb883514
Kiki Lamb <katherinem13@Syn.local>
2015-11-17 20:27:35 -0500

    ...

--- bubbles.rb
+++ bubbles.rb
@@ -4,7 +4,7 @@ require_relative "./lib/drumtool"
 include DrumTool
 
 easy_start(
-  Models::Bubbles::Preprocessors::Preprocessor,
+  Models::Bubbles::Standard::Preprocessors::Preprocessor,
   Models::Bubbles::Standard.track,
   "input/bubbles.dt2"
 )
