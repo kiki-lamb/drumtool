e82b2a7ba4e871910b599e38132ffb19251f9eb5
Kiki Lamb <katherinem13@Syn.local>
2015-11-18 18:15:46 -0500

    ...

--- lib/drumtool/playback/live_playback.rb
+++ lib/drumtool/playback/live_playback.rb
@@ -11,53 +11,53 @@ module DrumTool
     )
 		  super *a, **b
 
-      @reload_interval = reload_interval
+      @load_interval = reload_interval
 			@last_reload_time = nil
 			@last_reload_tick = 0
-		  @reloader = Loader.new(@filename, @preprocessor, rescue_exceptions: rescue_exceptions, init: init)
-			@reloader.after do |to| 
+		  @loader = Loader.new(@filename, @preprocessor, rescue_exceptions: rescue_exceptions, init: init)
+			@loader.after do |to| 
 			    @clock.tempo = to.bpm if to.bpm && @clock unless @input_clock
 			    to.bpm @clock.tempo unless to.bpm
-				  @reload_interval = to.refresh_interval if to.respond_to?(:refresh_interval)
+				  @load_interval = to.refresh_interval if to.respond_to?(:refresh_interval)
 			end
     end
 
 		private
 		def engine
-		  @reloader.payload
+		  @loader.payload
 		end
 
 		def tick
       reload
       
-      @reloader.safely_with_payload do
+      @loader.safely_with_payload do
 			  super
       end
 		end
 
 		def reload
-		  if @tick % @reload_interval == 0
-		    @last_reload_time = @reloader.reload
+		  if @tick % @load_interval == 0
+		    @last_reload_time = @loader.reload
 				@last_reload_tick = @tick unless 0 == @last_reload_time
 			else
-			  @last_reload_time = 00
+			  @last_reload_time = 0
       end
 		end
 		
 		def log_columns
 		  reload_time = 0
 			unchanged_bars = (@tick-@last_reload_tick)/16.0
-			reload_bars = @reload_interval/16.0
+			reload_bars = @load_interval/16.0
 			countdown = reload_bars-((@tick-@last_reload_tick)/16%reload_bars)
 			countdown = countdown%1.0 == 0  ? countdown.to_i : countdown.to_r
 
 		  [ 
-			  @reload_interval,
+			  @load_interval,
 			  "#{unchanged_bars.to_i.to_s.rjust(2)} bars", 
 			  "T-#{countdown} bars",
 				"#{@last_reload_time.to_s[0..5].rjust(6)} ms",
 				*super,
-        ("#{@reloader.exception_lines[@tick%(engine.loop || 16)].strip}" if @reloader.exception_lines.any?),
+        ("#{@loader.exception_lines[@tick%(engine.loop || 16)].strip}" if @loader.exception_lines.any?),
 			].compact
 		end		
   end 
