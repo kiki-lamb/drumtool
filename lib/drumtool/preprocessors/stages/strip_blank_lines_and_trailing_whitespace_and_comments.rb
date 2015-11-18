9d77c9a77b7ece3f0da0599b609baa2e61a8a65a
Kiki Lamb <katherinem13@Syn.local>
2015-11-18 00:57:02 -0500

    ...

--- lib/drumtool/preprocessors/stages/strip_blank_lines_and_trailing_whitespace_and_comments.rb
+++ lib/drumtool/preprocessors/stages/strip_blank_lines_and_trailing_whitespace_and_comments.rb
@@ -2,13 +2,12 @@ module DrumTool
 	module Preprocessors
 	  module Stages
 		  class StripBlankLinesAndTrailingWhitespaceAndComments < Base
-        def initialize p = nil, exclude: nil
+        def initialize *a, exclude: nil
           @exclude = exclude
-          super p
+          super *a
         end
 
         def call
-	    	  #text.gsub /(?:\s*(?:#[^\n]*)?\n)+/m, "\n"
           text.gsub /(?:\s*(?:##{"(?!#{@exclude})" if @exclude}[^\n]*)?\n)+/m, "\n"
 	    	end
 			end
