57ad4eb7802a96c23f02b9a0ea04b399531c97fd
Kiki Lamb <katherinem13@Syn.local>
2015-11-17 20:55:22 -0500

    ...

---
+++ lib/drumtool/models/bubbles/standard/preprocessors/stages/dumber_rubify_pythonesque_blocks.rb
@@ -0,0 +1,73 @@
+module DrumTool
+  module Models
+		class Bubbles
+      module Standard
+        module Preprocessors
+			    module Stages
+		        class DumberRubifyPythonesqueBlocks < DrumTool::Preprocessors::Stages::Base
+              class IndentationError < Exception; end
+              class BadBlockError < Exception; end
+              
+              LinePat = /^(\s*)(>)?\s*(.*)/
+              
+              def split line
+                LinePat.match line
+                [ Regexp.last_match[1], Regexp.last_match[2], Regexp.last_match[3] ]
+              end
+              
+              def initialize scope_method: :bubble, require_prefix: nil
+                @require_prefix = require_prefix
+                @scope_method = scope_method
+              end
+              
+	    	      def call
+	    	        prev_indents = [ 0 ]
+                lines = text.lines.each_with_index.map do |line, index|
+                  [ index, line, *split(line) ]
+                end
+                
+                half_lines = []
+                
+	    	        lines.each do |index, entire, indent, is_block_open, rest|            
+	    	          log "#{pad_number index} #{pad_number prev_indents.last, 2}->#{pad_number indent.length, 2} #{entire.chomp}"
+	    	          if prev_indents.last < indent.length
+	    	            prior = lines[index-1]
+                    raise BadBlockError, "Bad block open on line ##{index}" if @require_prefix && (prior[3].nil? || prior[3].empty?)
+                    
+	    	            log "#{pad_number index}        Blockify prior line `#{prior[1].chomp}'."
+                    
+                    half_lines.push [ index - 1.5, "#{prior[2]}#{@scope_method} do \n" ]
+	    	            lines[index-1][1] = "#{indent}#{prior[4]}\n"
+                    
+	    	            prev_indents.push indent.length
+	    	          elsif prev_indents.last > indent.length
+	    	            log "#{pad_number index}        Leave block"
+                    
+	    	            while prev_indents.last != indent.length
+	    	              begin                  
+                        raise IndentationError, "Bad outdent on line ##{index}" if prev_indents[-2].nil?
+	    	                lines[index-1][1] << "#{" " * prev_indents[-2]}end\n"
+	    	              rescue ArgumentError
+	    	                raise RuntimeError, "Bad unindent."
+	    	              end
+	    	              prev_indents.pop
+	    	            end
+	    	          end
+	    	        end
+                
+	    	        while prev_indents.last  != 0
+                  s = "#{" " * prev_indents[-2]}end\n"
+	    	          lines << [lines.length+1, s]
+	    	          prev_indents.pop
+	    	        end
+					      (lines + half_lines).sort_by(&:first).map do |l|
+                  l[1]
+                end.join
+	    	      end
+			      end
+		      end
+	      end
+      end
+    end
+  end
+end
