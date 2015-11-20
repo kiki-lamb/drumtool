b95bc368ef30326e9416f555cbf00bcff701064c
Kiki Lamb <katherinem13@Syn.local>
2015-11-20 03:13:37 -0500

    ...

---
+++ lib/drumtool/models/bubbles/traits/events/notes.rb
@@ -0,0 +1,29 @@
+module DrumTool
+  module Models
+		class Bubbles
+      module Traits
+        module Events
+		    module Notes
+          def note name, number = nil, velocity = nil, channel = nil
+            local_notes[name] = register_note name, number, velocity, channel
+          end
+
+          def notes
+            local_notes.values.to_a
+          end
+          
+          private				
+				  def local_events
+				    [ *local_notes.values.map(&:number), *super ]  # .tap { |x| puts "#{self} yields #{x.inspect}" }
+				  end
+
+          def local_notes
+            @__local_notes ||= {}
+          end
+			  end
+        end
+      end
+		end
+	end
+end
+
