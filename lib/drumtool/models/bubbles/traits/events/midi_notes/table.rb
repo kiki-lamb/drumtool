d680ab56aca1fc32069c8b881868e9877a3bd39a
Your Name <you@example.com>
2015-11-21 00:14:22 -0500

    ...

--- lib/drumtool/models/bubbles/traits/events/midi_notes/table.rb
+++ lib/drumtool/models/bubbles/traits/events/midi_notes/table.rb
@@ -4,58 +4,55 @@ module DrumTool
       module Traits
         module Events
           module MIDINotes
-            module Table
+            module Table # TODO: Come up with a better name for this class.
               def register_note note
-                note_registry[note.name].merge!(note)#.tap { |n| puts "N: #{n.inspect}" })
+                note_registry[note.name].merge!(note)
               end
-
-              def lookup note
-              if note_registry.include? note.name
-                note_registry[note.name]
-              elsif (n = note_registry.find do |reg_note|
-                       reg_note.number == note.number
-                     end)
-                n
-              elsif note.name || note.number
-                register_note note
-              end
-            end
-
-              def local_events
-                #puts "#{self} Table#local_events IN"
-                super.each do |evt|
-                  if MIDI::Note === evt
-                    evt.merge! lookup(evt) # .tap { |x| puts "#{evt.inspect} MERGE WITH #{x.inspect}" }
-                  end                                         
-                end#.tap {                 puts "#{self} Table#local_events OUT" }
-            end
-            
-            def displayed_notes
-              evts = events
-              evts_names = events.map &:name
-#              evts_nums = events.map(&:number).tap { |ns| puts "NUMS: #{ns}" }
               
-              note_registry.map do |k, v|
-                if evts_names.include?(v.name)
-                  evt = evts.find do |e|
-                    e.name == v.name
-                  end
-                  
-                  "#{k.to_s.ljust(2," ")} #{evt.number.to_s.ljust(2," ")}"
-                else
-                  nil
+              def lookup note
+                if note_registry.include? note.name
+                  note_registry[note.name]
+                elsif (n = note_registry.find do |reg_note|
+                         reg_note.number == note.number
+                       end)
+                  n
+                elsif note.name || note.number
+                  register_note note
                 end
               end
-            end
-            
-            def note_registry
-              @__note_registry ||= Hash.new { |h,k| h[k] = MIDINotes::EnhancedMIDINote.new(nil) }
+              
+              def local_events
+                super.each do |evt|
+                  if MIDI::Note === evt
+                    evt.merge! lookup(evt) 
+                  end                                         
+                end
+              end
+              
+              def displayed_notes
+                evts = events
+                evts_names = events.map &:name
+                
+                note_registry.map do |k, v|
+                  if evts_names.include?(v.name)
+                    evt = evts.find do |e|
+                      e.name == v.name
+                    end
+                    
+                    "#{k.to_s.ljust(2," ")} #{evt.number.to_s.ljust(2," ")}"
+                  else
+                    nil
+                  end
+                end
+              end
+              
+              def note_registry
+                @__note_registry ||= Hash.new { |h,k| h[k] = MIDINotes::EnhancedMIDINote.new(nil) }
+              end
             end
           end
         end
       end
     end
   end
-end
-
 end
