1f3cd92540b2d4e4507f4d0ea3878b7d5b8872f1
Your Name <you@example.com>
2015-11-21 00:33:28 -0500

    ...

--- lib/drumtool/models/basic.rb
+++ lib/drumtool/models/basic.rb
@@ -1,6 +1,9 @@
 module DrumTool
   module Models
     module Basic
+      # The classes in this module are pretty obsolete by this point.
+      # This module will probably be removed soon.
+
       class << self
         def build &b
           TimingScope.new &b
