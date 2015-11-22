a2e5aace80410c23d45b78836183f4b499b38090
Your Name <you@example.com>
2015-11-21 20:47:10 -0500

    ...

--- dt.rb
+++ dt.rb
@@ -4,22 +4,10 @@ require_relative "./lib/drumtool"
 include DrumTool
 include DrumTool::Models::Bubbles::Standard
 
-Preprocessors::Preprocessor.log_to  "output/preprocessor"
-
 easy_start(
-  nil, #Preprocessors::Preprocessor,
+  Preprocessors::Preprocessor,
   track,
   ARGV[0] || "input/new.dt2",
-  #UniMIDI::Input[1],
-  rescue_exceptions: false,
-  output: UniMIDI::Output[0],
-  # logs: [],
-  preprocessor_logs: ["output/preprocessor", $stdout],
-  loader: MultiLoader.new(
-    Preprocessors::Preprocessor,
-    "input/new2.dt2",
-#    "input/bubbles2.dt2",
-    init: track,
-    rescue_exceptions: false
-    )
+  false,
+  rescue_exceptions: false
 )
