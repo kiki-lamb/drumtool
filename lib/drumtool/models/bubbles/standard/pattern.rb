031aff4315d3fa6b5d44ceafb6b959cb111524b9
Your Name <you@example.com>
2015-11-25 04:58:11 -0500

    Try some indentation out

--- lib/drumtool/models/bubbles/standard/pattern.rb
+++ lib/drumtool/models/bubbles/standard/pattern.rb
@@ -8,18 +8,18 @@ module DrumTool
           prepend Time::Relative::EnhancedLooping
 
           prepend Events::MIDINotes
-          include Events::MIDINotes::Helpers::ScaleNotes
+            include Events::MIDINotes::Helpers::ScaleNotes
 
           prepend Events::Transform
           prepend Events::Transform::WithContextSetVia[:parent=]
-          include Events::MIDINotes::Transform::Rename
-          include Events::MIDINotes::Transform::Remap
-          include Events::MIDINotes::Transform::Transpose
-          include Events::MIDINotes::Transform::ToScale
+            include Events::MIDINotes::Transform::Rename
+            include Events::MIDINotes::Transform::Remap
+            include Events::MIDINotes::Transform::Transpose
+            include Events::MIDINotes::Transform::ToScale
           
           prepend Events::Filter          
-          include Events::MIDINotes::Filter::MinMax
-          include Events::MIDINotes::Filter::InScale
+            include Events::MIDINotes::Filter::MinMax
+            include Events::MIDINotes::Filter::InScale
 
           prepend Events::Triggered
           prepend Events::Muteable
