03cc07956b5a9f72e614623afd962c2f170167dd
Your Name <you@example.com>
2015-11-25 15:35:23 -0500

    ...

--- lib/drumtool/models/bubbles/traits/events/midi_notes/transform/to_scale.rb
+++ lib/drumtool/models/bubbles/traits/events/midi_notes/transform/to_scale.rb
@@ -6,15 +6,18 @@ module DrumTool
           module MIDINotes
             module Transform
               module ToScale
-                def to_scale note_name, type = :minor, modsym = :+, *a
-                  remap *map_for_scale(scale_notes(note_name, type, *a, ordered: true), mod_from_sym(modsym))
+                def to_scale *a
+                  __to_scale__ true, *a
                 end
 
-                def to_scale! note_name, type = :minor, modsym = :+, *a
-                  remap *map_for_scale(scale_notes(note_name, type, *a, ordered: false), mod_from_sym(modsym))
+                def to_scale! *a
+                  __to_scale__ false, *a
                 end
 
                 private
+                def __to_scale__ ordered, note_name, type = :minor, modsym = :+, *a
+                  remap *map_for_scale(scale_notes(note_name, type, *a, ordered: ordered), mod_from_sym(modsym))
+                end
 
                 def mod_from_sym sym
                   if :+ == sym
@@ -26,16 +29,10 @@ module DrumTool
                   end
                 end
                 
-                def note_to_scale note, in_notes, mod = 1
-                  until in_notes.include?(note) || in_notes.include?(note-(12*mod))
-                    note += mod
-                  end
-                  note
-                end
-                
                 def map_for_scale scale_notes, mod = 1
                   (0..11).map do |note|
-                    note_to_scale(note + scale_notes.first, scale_notes, mod) 
+                    note += mod until scale_notes.include?(note) || scale_notes.include?(note-12*mod)
+                    note
                   end
                 end                
               end
