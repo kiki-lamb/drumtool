5b2fa68b793b75edc4459c9b9b5a5e2071babb49
Your Name <you@example.com>
2015-11-25 04:33:53 -0500

    quiet

--- lib/drumtool/models/bubbles/traits/events/midi_notes/filter.rb
+++ lib/drumtool/models/bubbles/traits/events/midi_notes/filter.rb
@@ -16,8 +16,6 @@ module DrumTool
               def events
                 return super if __filter_actions__.empty?
 
-                puts "APPLY #{__filter_actions__.count} FILTERS"
-                
                 super.dup.tap do |evts|
                   __filter_actions__.each do |action|
                     evts.select! &action
