c6cd8125a4be99cf48a2789438631af61db036c4
Your Name <you@example.com>
2015-11-25 17:20:33 -0500

    ...g

--- lib/drumtool/models/bubbles/traits/events/midi_notes/helpers/scale_notes.rb
+++ lib/drumtool/models/bubbles/traits/events/midi_notes/helpers/scale_notes.rb
@@ -11,16 +11,13 @@ module DrumTool
                   (Note.new(note_name.to_s)-(@__down__ = NoteInterval.new(60)))
                 end
                 
-                def scale_notes note_name, type = :minor, *a, unrooted: false
+                def scale_notes note_name, type = :minor, *a
                   tmp = if Fixnum === note_name
                           [ note_name, type, *a ]
                         else
                           root = root_note note_name
                           root.send("#{type}_scale").note_values.map { |x| x % 12 }.sort
                         end
-
-                  tmp.push tmp.shift+12 until tmp.first == root.value if unrooted
-
                   tmp
                 end
               end
--- lib/drumtool/models/bubbles/traits/events/midi_notes/transform/to_scale.rb
+++ lib/drumtool/models/bubbles/traits/events/midi_notes/transform/to_scale.rb
@@ -6,19 +6,30 @@ module DrumTool
           module MIDINotes
             module Transform
               module ToScale
-                def rescale! *a
+                def to_scale! *a
                   __to_scale__ true, *a
                 end
 
-                def to_scale! *a
+                def rescale! *a
                   __to_scale__ false, *a
                 end
 
                 private
-                def __to_scale__ ordered, note_name, type = :minor, modsym = :+, *a
-                  map = map_for_scale(scale_notes(note_name, type, *a, ordered: ordered), mod_from_sym(modsym))
-                  puts "MAP: #{map.inspect}"
-                  remap! *map
+                def __to_scale__ ordered, note_name, type = :minor, modsym = :-, *a
+                  remap! *map_for_scale((ordered ? root_note(note_name).value : 0), scale_notes(note_name, type, *a), mod_from_sym(modsym))
+                end
+                
+                def map_for_scale root, scale_notes, mod = 1
+                  tmp = (0..11).map do |note|
+                    note += mod until scale_notes.include?(note) || scale_notes.include?(note-12*mod)
+                    note
+                  end
+                  
+                  until tmp.first == root
+                    tmp.push tmp.shift+12
+                  end
+                  
+                  tmp
                 end
 
                 def mod_from_sym sym
@@ -30,13 +41,6 @@ module DrumTool
                     raise ArgumentError, "Invalid mod symbol"
                   end
                 end
-                
-                def map_for_scale scale_notes, mod = 1
-                  (0..11).map do |note|
-                    note += mod until scale_notes.include?(note) || scale_notes.include?(note-12*mod)
-                    note
-                  end
-                end                
               end
             end
           end
