a143d8d78f8fca49dc75f736181d7f547952269d
Your Name <you@example.com>
2015-11-26 15:21:58 -0500

    ...

--- lib/drumtool/models/bubbles/standard/pattern.rb
+++ lib/drumtool/models/bubbles/standard/pattern.rb
@@ -10,16 +10,16 @@ module DrumTool
 
           prepend Events::Transform
           prepend Events::Transform::WithContextSetVia[:parent=]
-            include Events::MIDI::Notes::Transform::Attributes
-            include Events::MIDI::Notes::Transform::Rename
-            include Events::MIDI::Notes::Transform::Transpose
-            include Events::MIDI::Notes::Transform::Remap
-            include Events::MIDI::Notes::Transform::Remap::ToScale
-            include Events::MIDI::Controllers::Transform::Attributes
+          include Events::MIDI::Notes::Transform::Attributes
+          include Events::MIDI::Notes::Transform::Rename
+          include Events::MIDI::Notes::Transform::Transpose
+          include Events::MIDI::Notes::Transform::Remap
+          include Events::MIDI::Notes::Transform::Remap::ToScale
+          include Events::MIDI::Controllers::Transform::Attributes
 
           prepend Events::Filter
-            include Events::Filter::MinMax
-            include Events::MIDI::Notes::Filter::InScale            
+          include Events::Filter::MinMax
+          include Events::MIDI::Notes::Filter::InScale            
 
           prepend Events::Triggered
           prepend Events::Muteable
